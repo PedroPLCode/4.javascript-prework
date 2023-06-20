79328ca568994cb278ae9b4488ab4deaa9cb857a
Piotr Gaszczynski <piotrek.gaszczynski@gmail.com>
2023-06-20 23:00:03 +0200

    Add js functions

--- js/script.js
+++ js/script.js
@@ -12,3 +12,18 @@ var computerMove;
 computerMove = 'kamień';
 playerMove - 'papier;'
 printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to ' + playerMone + ' , to wygrywasz!');
+
+var computerMove, randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('wylosowana liczba to: ' + randomNumber);
+if (randomNumber == '1') {
+  computerMove = 'kamień';
+} 
+else if (randomNumber == '2') {
+    computerMove = 'papier';
+} 
+else {
+  computerMove = 'nożyce';
+}
+printMessage('Mój ruch: ' + computerMove);
+

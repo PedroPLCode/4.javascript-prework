f317fd86e34e85c29480539b0637627baaccf41f
Piotr Gaszczynski <piotrek.gaszczynski@gmail.com>
2023-06-20 23:05:36 +0200

    Add js PlayerMove functions

--- js/script.js
+++ js/script.js
@@ -17,13 +17,31 @@ var computerMove, randomNumber;
 randomNumber = Math.floor(Math.random() * 3 + 1);
 console.log('wylosowana liczba to: ' + randomNumber);
 if (randomNumber == '1') {
-  computerMove = 'kamień';
+    computerMove = 'kamień';
 } 
 else if (randomNumber == '2') {
     computerMove = 'papier';
 } 
 else {
-  computerMove = 'nożyce';
+    computerMove = 'nożyce';
 }
 printMessage('Mój ruch: ' + computerMove);
+
+var playerMove, playerInput;
+playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Wpisana odpowiedź to: ' + playerInput);
+if (playerInput == '1') {
+    playerMove = 'kamień';
+}
+else if (playerInput == '2') {
+    playerMove = 'papier';
+}
+else if (playerInput == '3') {
+    playerMove = 'nożyce';
+}
+else {
+  playerMove = 'nieznany ruch';
+  printMessage('Błąd! Podaj liczbę z zakresy 1-3.')
+}
+printMessage('Twój ruch: ' + playerMove);
 

386db0cda36872699dd187c9e3569e8b3624b619
Piotr Gaszczynski <piotrek.gaszczynski@gmail.com>
2023-06-22 21:57:40 +0200

    Apply final chages to html css and js

--- js/functions.js
+++ js/functions.js
@@ -1,93 +1,89 @@
-function printMessage(msg){
+function printMessage(msg) {
 	var div = document.createElement('div');
 	div.innerHTML = msg;
 	document.getElementById('messages').appendChild(div);
 }
 
-function clearMessages(){
+function clearMessages() {
 	document.getElementById('messages').innerHTML = '';
 }
 
 /**
- * Describe this function...
+ * Gets move id and retuns move name as a string.
  */
 function getMoveName(argMoveId) {
-    console.log('wywołano funkcję getMoveName z argumentem: ' + argMoveId);
-    if (argMoveId == 1) {
-      return 'kamień';
-    } 
-    else if (argMoveId == 2) {
-      return 'papier';
-    }
-    else if (argMoveId == 3) {
-      return 'nożyce';
-    }
-    else {
-      printMessage('Nie znam ruchu o id ' + argMoveId + '. Zakładam, że chodziło o "kamień".');
-      return 'kamień';
-    }
+  console.log('wywołano funkcję getMoveName z argumentem: ' + argMoveId);
+  if (argMoveId == 1) {
+    return 'rock';
+  } else if (argMoveId == 2) {
+    return 'paper';
+  } else if (argMoveId == 3) {
+    return 'scissors';
+  } else {
+    printMessage('Nie znam ruchu o id ' + argMoveId + '. Zakładam, że chodziło o "kamień".');
+    return 'rock';
   }
-  
-  
-  function displayResult(argPlayerMove, argComputerMove, argScorePlayer, argScoreComputer, argCounter) {
-      console.log('wywołano funkcję displayResults z argumentami: ' + argPlayerMove + ', ' + argComputerMove);
-      if (argPlayerMove == 'papier' && argComputerMove == 'kamień') {
-          printMessage('Wygrywasz!');
-          scorePlayer++;
-          counter++;
-      }
-      else if (argPlayerMove == 'kamień' && argComputerMove == 'nożyce') {
-          printMessage('Wygrywasz!');
-          scorePlayer++;
-          counter++;
-      }
-      else if (argPlayerMove == 'nożyce' && argComputerMove == 'papier') {
-          printMessage('Wygrywasz!');
-          scorePlayer++;
-          counter++;
-      }
-      else if (argPlayerMove == argComputerMove) {
-          printMessage('Mamy remis.');
-          counter++;
-      }
-      else {
-          printMessage('Przegrywasz.');
-          scoreComputer++;
-          counter++;
-      }
-      printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
-      printMessage('Round: ' + argCounter);
-      printMessage('Scoreboard:')
-      printMessage('Human: ' + argScorePlayer + ' / Computer: ' + argScoreComputer);
-      printMessage('Play again!')
-      //return counter, scorePlayer, scoreComputer;
-  }
+}
 
-  function resetCounter(argScorePlayer, argScoreComputer, argCounter) {
-    console.log('reset klikniety');
-    scorePlayer = 0;
-    scoreComputer = 0;
-    counter = 0;
-    console.log('counter' + counter + ' player ' + scorePlayer + ' comp ' + scoreComputer);
-    clearMessages();
-    //displayResult(playerMove, computerMove, argScorePlayer, argScoreComputer, argCounter);
+/**
+ * Makes decision about result of the game and displays on the screen.
+ */  
+function displayResult(argPlayerMove, argComputerMove, argScorePlayer, argScoreComputer, argCounter) {
+  console.log('wywołano funkcję displayResults z argumentami: ' + argPlayerMove + ', ' + argComputerMove);
+  if (argPlayerMove == 'paper' && argComputerMove == 'rock') {
+      printMessage('YOU WON!');
+      scorePlayer++;
+      counter++;
+  } else if (argPlayerMove == 'rock' && argComputerMove == 'scissors') {
+    printMessage('YOU WON!');
+      scorePlayer++;
+      counter++;
+  } else if (argPlayerMove == 'scissors' && argComputerMove == 'paper') {
+    printMessage('YOU WON!');
+       scorePlayer++;
+      counter++;
+  } else if (argPlayerMove == argComputerMove) {
+    printMessage('REMIS');
+      counter++;
+  } else {
+      printMessage('YOU LOSE');
+      scoreComputer++;
+      counter++;
+  }
+  printMessage('ai had ' + argComputerMove + ', human had ' + argPlayerMove);
+  printMessage('round: ' + counter);
+  printMessage('scoreboard:')
+  printMessage('human: ' + scorePlayer + ' / computer: ' + scoreComputer);
+  printMessage('play again!');
+}
+
+/**
+ * Reaction to reset button clicked by user. Clear scores and round counter.
+ */
+function resetCounter(argScorePlayer, argScoreComputer, argCounter) {
+  console.log('reset klikniety');
+  scorePlayer = 0;
+  scoreComputer = 0;
+  counter = 0;
+  console.log('counter ' + counter + ' player ' + scorePlayer + ' comp ' + scoreComputer);
+  clearMessages();
+  printMessage('reset')
+  printMessage('counter clear')
+  printMessage('scoreboard clear')
   }
 
 /**
- * Describe this function...
+ * Reaction to button clicked by user.
  */
 function buttonClicked(argButtonName, argScorePlayer, argScoreComputer, argCounter) {
-    clearMessages();
-    console.log(argButtonName + ' został kliknięty');
-
-    // resetCounter(scorePlayer, scoreComputer, counter);
-
-    playerMove = argButtonName;
-    console.log('wybór ruchu gracza to: ' + playerInput);
-    console.log('ruch gracza to: ' + playerMove);
-    randomNumber = Math.floor(Math.random() * 3 + 1);
-    console.log('wylosowana liczba to: ' + randomNumber);
-    computerMove = getMoveName(randomNumber);
-    console.log('ruch komputera to: ' + computerMove);
-    displayResult(playerMove, computerMove, argScorePlayer, argScoreComputer, argCounter);
+  clearMessages();
+  console.log(argButtonName + ' został kliknięty');
+  playerMove = argButtonName;
+  console.log('wybór ruchu gracza to: ' + playerInput);
+  console.log('ruch gracza to: ' + playerMove);
+  randomNumber = Math.floor(Math.random() * 3 + 1);
+  console.log('wylosowana liczba to: ' + randomNumber);
+  computerMove = getMoveName(randomNumber);
+  console.log('ruch komputera to: ' + computerMove);
+  displayResult(playerMove, computerMove, argScorePlayer, argScoreComputer, argCounter);
 }
--- js/script.js
+++ js/script.js
@@ -3,14 +3,13 @@ var argButtonName, buttonRock, buttonPaper, buttonScissors;
 var scorePlayer = 0, scoreComputer = 0, counter = 0;
 
 buttonRock = document.getElementById('button-rock');
-buttonRock.addEventListener('click', function(){ buttonClicked('kamień', scorePlayer, scoreComputer, counter); });
+buttonRock.addEventListener('click', function(){ buttonClicked('rock', scorePlayer, scoreComputer, counter); });
 
 buttonPaper = document.getElementById('button-paper');
-buttonPaper.addEventListener('click', function(){ buttonClicked('papier', scorePlayer, scoreComputer, counter); });
+buttonPaper.addEventListener('click', function(){ buttonClicked('paper', scorePlayer, scoreComputer, counter); });
 
 buttonScissors = document.getElementById('button-scissors');
-buttonScissors.addEventListener('click', function(){ buttonClicked('nożyce', scorePlayer, scoreComputer, counter); });
+buttonScissors.addEventListener('click', function(){ buttonClicked('scissors', scorePlayer, scoreComputer, counter); });
 
 buttonReset = document.getElementById('button-reset');
 buttonReset.addEventListener('click', function(){ resetCounter(scorePlayer, scoreComputer, counter); });
-
